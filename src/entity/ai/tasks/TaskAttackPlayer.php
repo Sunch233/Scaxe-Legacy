5ec8f2b345705e0c673db3fce765c41a72d4c455
qwqer22 <159139196+qwqer22@users.noreply.github.com>
2025-03-22 17:20:04 +0800

    Update TaskAttackPlayer.php
    Prevent Creatures from Pushing Players in Creation Mode

--- src/entity/ai/tasks/TaskAttackPlayer.php
+++ src/entity/ai/tasks/TaskAttackPlayer.php
@@ -26,8 +26,10 @@ class TaskAttackPlayer extends TaskBase
 			return false;
 		}
 	if($ai->entity->closestPlayerToAttackDist>=3)
-{	$ai->mobController->setMovingTarget($ai->entity->target->x, $ai->entity->target->y, $ai->entity->target->z, $this->speedMultiplier);
-	}	$ai->mobController->setLookPosition($ai->entity->target->x, $ai->entity->target->y + 0.12, $ai->entity->target->z, 10, $ai->entity->getVerticalFaceSpeed());
+{
+	$ai->mobController->setMovingTarget($ai->entity->target->x, $ai->entity->target->y, $ai->entity->target->z, $this->speedMultiplier);
+}	
+	$ai->mobController->setLookPosition($ai->entity->target->x, $ai->entity->target->y + 0.12, $ai->entity->target->z, 10, $ai->entity->getVerticalFaceSpeed());
 		
 		--$this->attackCounter;
 		$v1 = $ai->entity->width * $ai->entity->width * 4;
